7c9dbd03cc63a96e00939258ee9f41bf70a0d3dc
Omar Naguib <55961349+OmarNaguib@users.noreply.github.com>
2022-11-05 07:55:19 +0200

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -4,3 +4,7 @@ np.zeros_like()
 #2
 # sort the results, highest prediction first
 sorted_index = np.argsort(-y_pu,axis=0).reshape(-1).tolist()  #negate to get largest rating first
+
+#3
+#course 3 week 2 lab 2
+m_dist = ma.masked_array(dist, mask=np.identity(dist.shape[0]))  # mask the diagonal
